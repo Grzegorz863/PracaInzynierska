ea3172791d0e14b3034d428398305a74fb1d3e2e
Grzegorz863 <gn22@o2.pl>
2018-07-27 18:51:04 +0200

    added some catalogs

--- tcps-server/src/main/java/pl/tcps/controllers/WitamController.java
+++ tcps-server/src/main/java/pl/tcps/controllers/WitamController.java
@@ -1,11 +1,13 @@
 package pl.tcps;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WitamController {
 
+    @GetMapping("/")
     @RequestMapping("/")
     public String costam (){
         return "teścik";
